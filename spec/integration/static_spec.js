e0eaeccdf34ff5be67bb03cf809960cce4d6c967
Norman Desch <normandesch3@gmaill.com>
2019-06-14 16:34:27 -0400

    app.set error

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -4,17 +4,17 @@ const base = "http://localhost:3000/";
 
 describe("routes : static", () => {
 
-//#1
+
   describe("GET /", () => {
 
-//#2
-    it("should return status code 200", (done) => {
 
-//#3
+    it("should return status code 200 and have 'Welcome to Blocipedia' in the body of the response", (done) => {
+
+
+
       request.get(base, (err, res, body) => {
         expect(res.statusCode).toBe(200);
-
-//#4
+        expect(body).toContain("Welcome to Blocipedia");
         done();
       });
     });
--- src/server.js
+++ src/server.js
@@ -1,10 +1,12 @@
 const app = require("./app");
 const http = require("http");
-const port = normalizePort(process.env.PORT || "3000");
 
+
+const port = normalizePort(process.env.PORT || "3000");
 app.set("port", port);
 
 const server = http.createServer(app);
+
 
 server.listen(port);
 
@@ -19,6 +21,6 @@ function normalizePort(val) {
    return false;
  }
 
-server.on("listening", () => {
-console.log(`server is listening for requests on port ${server.address().port}`);
-});
+ server.on("listening", () => {
+   console.log(`server is listening for requests on port ${server.address().port}`);
+ });

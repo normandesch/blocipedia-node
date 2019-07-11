33c711f283f9a8ad3dc94c473e1bcba0110cafaf
desknor <45925700+normandesch@users.noreply.github.com>
2019-07-11 12:32:02 -0400

    Update wikis_spec.js

--- spec/integration/wikis_spec.js
+++ spec/integration/wikis_spec.js
@@ -1,147 +1,89 @@
-const request = require("request");
-const server = require("../../src/server");
-const base = "http://localhost:3000/wikis";
 const sequelize = require("../../src/db/models/index").sequelize;
 const User = require("../../src/db/models").User;
 const Wiki = require("../../src/db/models").Wiki;
 
-describe("routes:wikis", () => {
+describe("Wiki", () => {
   beforeEach(done => {
     this.user;
     this.wiki;
-    sequelize.sync({ force: true }).then(() => {
+    sequelize.sync({ force: true }).then(res => {
       User.create({
-        username: "Ben Dale",
-        email: "BenDale@gmail.com",
+        username: "Samantha Pacheco",
+        email: "pacheco@gmail.com",
         password: "123456789"
       }).then(user => {
         this.user = user;
 
         Wiki.create({
-          title: "Dragon Lair",
-          body: "If you love dragons, come here or burn!",
+          title: "Harps",
+          body: "If you love harps, come here!",
           private: false,
           userId: this.user.id
-        })
-          .then(wiki => {
-            this.wiki = wiki;
-            done();
-          })
-          .catch(err => {
-            console.log(err);
-            done();
-          });
-      });
-    });
-  });
-
-  describe("GET /wikis", () => {
-    it("should return a status code 200 and all wikis", done => {
-      request.get(base, (err, res, body) => {
-        expect(res.statusCode).toBe(200);
-        expect(err).toBeNull();
-        expect(body).toContain("Wikis");
-        expect(body).toContain("Dragon Lair");
-        done();
-      });
-    });
-  });
-
-  describe("GET /wikis/new", () => {
-    it("should render a new wiki form", done => {
-      request.get(`${base}/new`, (err, res, body) => {
-        expect(err).toBeNull();
-        expect(body).toContain("New Wiki");
-        done();
-      });
-    });
-  });
-
-  describe("POST /wikis/create", () => {
-    const options = {
-      url: `${base}/create`,
-      form: {
-        title: "Pineapple Palace",
-        body: "Pineapple Palace is where the cool men hang out.",
-        private: false,
-        userId: this.user.id
-      }
-    };
-    it("should create a new wiki and redirect", done => {
-      request.post(options, (err, res, body) => {
-        Wiki.findOne({ where: { title: "Pineapple Palace" } })
-          .then(wiki => {
-            expect(res.statusCode).toBe(303);
-            expect(wiki.title).toBe("Pineapple Palace");
-            expect(wiki.body).toBe(
-              "Pineapple Palace is where the cool women hang out."
-            );
-            done();
-          })
-          .catch(err => {
-            console.log(err);
-            done();
-          });
-      });
-    });
-  });
-
-  describe("GET /wikis/:id", () => {
-    it("should render a view with the selected wiki", done => {
-      request.get(`${base}/${this.wiki.id}`, (err, res, body) => {
-        expect(err).toBeNull();
-        expect(body).toContain("Dragon Lair");
-        done();
-      });
-    });
-  });
-
-  describe("POST /wikis/:id/destroy", () => {
-    it("should delete the wiki with the associated id", done => {
-      Wiki.findAll().then(wikis => {
-        const wikiCountBeforeDelete = wikis.length;
-
-        expect(wikiCountBeforeDelete).toBe(1);
-
-        request.post(`${base}/${this.wiki.id}/destroy`, (err, res, body) => {
-          Wiki.findAll().then(wikis => {
-            expect(err).toBeNull();
-            expect(wikis.length).toBe(wikiCountBeforeDelete - 1);
-            done();
-          });
+        }).then(wiki => {
+          this.wiki = wiki;
+          done();
         });
       });
     });
   });
 
-  describe("GET /wikis/:id/edit", () => {
-    it("should render a view with an edit wiki form", done => {
-      request.get(`${base}/${this.wiki.id}/edit`, (err, res, body) => {
-        expect(err).toBeNull();
-        expect(body).toContain("Edit Wiki");
-        expect(body).toContain("Dragon Lair");
-        done();
+  describe("#create()", () => {
+    it("should create a wiki object with a title,body,privacy status and assigned user", done => {
+      Wiki.create({
+        title: "Cat Shorts",
+        body: "We love clothes made of cat hair!",
+        private: false,
+        userId: this.user.id
+      })
+        .then(wiki => {
+          expect(wiki.title).toBe("Cat Shorts");
+          expect(wiki.body).toBe("We love clothes made of cat hair!");
+          expect(wiki.private).toBeFalsy();
+          expect(wiki.userId).toBe(this.user.id);
+          done();
+        })
+        .catch(err => {
+          console.log(err);
+          done();
+        });
+    });
+
+    it("should not create a wiki object with a missing title, body or assigned user", done => {
+      Wiki.create({
+        title: "Cat Shorts"
+      })
+        .then(wiki => {
+          done();
+        })
+        .catch(err => {
+          expect(err.message).toContain("Wiki.body cannot be null");
+          expect(err.message).toContain("Wiki.private cannot be null");
+          expect(err.message).toContain("Wiki.userId cannot be null");
+          done();
+        });
+    });
+
+    describe("#setUser()", () => {
+      it("should associate a user and a wiki together", done => {
+        User.create({
+          username: "Normandesk",
+          email: "ndesk@gmail.com",
+          password: "1234567890"
+        }).then(newUser => {
+          expect(this.wiki.userId).toBe(this.user.id);
+
+          this.wiki.setUser(newUser).then(wiki => {
+            expect(wiki.userId).toBe(newUser.id);
+            done();
+          });
+        });
       });
     });
-  });
 
-  describe(" POST /wikis/:id/update", () => {
-    it("should update the wiki with the given values", done => {
-      const options = {
-        url: `${base}/${this.wiki.id}/update`,
-        form: {
-          title: "Dragon Lair",
-          body: "If you love donuts, come here or else!",
-          private: false,
-          userId: this.user.id
-        }
-      };
-      request.post(options, (err, res, body) => {
-        expect(err).toBeNull();
-        Wiki.findOne({
-          where: { id: this.wiki.id }
-        }).then(wiki => {
-          expect(wiki.title).toBe("Dragon Lair");
+    describe("getUser", () => {
+      it("should return the associated user", done => {
+        this.wiki.getUser().then(associatedUser => {
+          expect(associatedUser.username).toBe("Samantha Pacheco");
           done();
         });
       });

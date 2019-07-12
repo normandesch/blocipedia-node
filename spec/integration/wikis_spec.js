bf58989f5de2064edb40f70cf1c5fe7b92c4903f
Norman Desch <normandesch3@gmaill.com>
2019-07-12 19:50:02 -0400

    crud redo

---
+++ spec/integration/wikis_spec.js
@@ -0,0 +1,150 @@
+const request = require("request");
+const server = require("../../src/server");
+const base = "http://localhost:3000/wikis";
+const sequelize = require("../../src/db/models/index").sequelize;
+const User = require("../../src/db/models").User;
+const Wiki = require("../../src/db/models").Wiki;
+
+describe("routes:wikis", () => {
+  beforeEach(done => {
+    this.user;
+    this.wiki;
+    sequelize.sync({ force: true }).then(() => {
+      User.create({
+        username: "Daniel Abelson",
+        email: "dable@gmail.com",
+        password: "123456789"
+      }).then(user => {
+        this.user = user;
+
+        Wiki.create({
+          title: "Dragon Liar",
+          body: "If you love dragons, come here or burn",
+          private: false,
+          userId: this.user.id
+        })
+          .then(wiki => {
+            this.wiki = wiki;
+            done();
+          })
+          .catch(err => {
+            console.log(err);
+            done();
+          });
+      });
+    });
+  });
+
+  describe("GET /wikis", () => {
+    it("should return a status code 200 and all wikis", done => {
+      request.get(base, (err, res, body) => {
+        expect(res.statusCode).toBe(200);
+        expect(err).toBeNull();
+        expect(body).toContain("Wikis");
+        expect(body).toContain("Dragon Liar");
+        done();
+      });
+    });
+  });
+
+  describe("GET /wikis/new", () => {
+    it("should render a new wiki form", done => {
+      request.get(`${base}/new`, (err, res, body) => {
+        expect(err).toBeNull();
+        expect(body).toContain("New Wiki");
+        done();
+      });
+    });
+  });
+
+  describe("POST /wikis/create", () => {
+    const options = {
+      url: `${base}/create`,
+      form: {
+        title: "Pineapple House",
+        body: "Pineapple House is where the cool people hang out.",
+        private: false,
+        userId: this.user.id
+      }
+    };
+    it("should create a new wiki and redirect", done => {
+      request.post(options, (err, res, body) => {
+        Wiki.findOne({ where: { title: "Pineapple House" } })
+          .then(wiki => {
+            expect(res.statusCode).toBe(303);
+            expect(wiki.title).toBe("Pineapple House");
+            expect(wiki.body).toBe(
+              "Pineapple House is where the cool people hang out."
+            );
+            done();
+          })
+          .catch(err => {
+            console.log(err);
+            done();
+          });
+      });
+    });
+  });
+
+  describe("GET /wikis/:id", () => {
+    it("should render a view with the selected wiki", done => {
+      request.get(`${base}/${this.wiki.id}`, (err, res, body) => {
+        expect(err).toBeNull();
+        expect(body).toContain("Dragon Liar");
+        done();
+      });
+    });
+  });
+
+  describe("POST /wikis/:id/destroy", () => {
+    it("should delete the wiki with the associated id", done => {
+      Wiki.findAll().then(wikis => {
+        const wikiCountBeforeDelete = wikis.length;
+
+        expect(wikiCountBeforeDelete).toBe(1);
+
+        request.post(`${base}/${this.wiki.id}/destroy`, (err, res, body) => {
+          Wiki.findAll().then(wikis => {
+            expect(err).toBeNull();
+            expect(wikis.length).toBe(wikiCountBeforeDelete - 1);
+            done();
+          });
+        });
+      });
+    });
+  });
+
+  describe("GET /wikis/:id/edit", () => {
+    it("should render a view with an edit wiki form", done => {
+      request.get(`${base}/${this.wiki.id}/edit`, (err, res, body) => {
+        expect(err).toBeNull();
+        expect(body).toContain("Edit Wiki");
+        expect(body).toContain("Dragon Liar");
+        done();
+      });
+    });
+  });
+
+  describe(" POST /wikis/:id/update", () => {
+    it("should update the wiki with the given values", done => {
+      const options = {
+        url: `${base}/${this.wiki.id}/update`,
+        form: {
+          title: "Dragon Liar",
+          body: "If you love dragons, come here or burn",
+          private: false,
+          userId: this.user.id
+        }
+      };
+      request.post(options, (err, res, body) => {
+        expect(err).toBeNull();
+        Wiki.findOne({
+          where: { id: this.wiki.id }
+        }).then(wiki => {
+          expect(wiki.title).toBe("Dragon Liar");
+          done();
+        });
+      });
+    });
+  });
+});
--- src/config/route-config.js
+++ src/config/route-config.js
@@ -1,9 +1,11 @@
 module.exports = {
   init(app){
     const staticRoutes = require("../routes/static");
+    const wikiRoutes = require("../routes/wikis");
     const userRoutes = require("../routes/users");
 
     app.use(staticRoutes);
+    app.use(wikiRoutes);
     app.use(userRoutes);
   }
 }

4501bd8ee4ce39437fd03d0bea2e3742fbceec97
Norman Desch <normandesch3@gmaill.com>
2019-08-21 23:42:19 -0400

    error message fix

--- src/controllers/collaboratorController.js
+++ src/controllers/collaboratorController.js
@@ -10,7 +10,7 @@ module.exports = {
   add(req, res, next) {
     collaboratorQueries.add(req, (err, collaborator) => {
       if (err) {
-        req.flash("error", err);
+        req.flash("User not valid.");
       }
       res.redirect(req.headers.referer);
     });
